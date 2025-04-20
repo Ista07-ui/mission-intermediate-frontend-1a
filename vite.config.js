56a2cb44ce58efd107c50b3068a585daf576b111
Ista07-ui <i.c.k.85995@gmail.com>
2025-04-21 05:18:57 +0700

    mengedit config.js dan home.css

--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -26,5 +25,8 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
